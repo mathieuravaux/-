acd8ca7c9f8062b3f91ff539c6d3ade63d4dfadd
Mathieu Ravaux <mathieu.ravaux@gmail.com>
2015-08-23 18:35:21 +0200

    Pusher stuff cleanup

--- server.rb
+++ server.rb
@@ -2,20 +2,7 @@ require 'uri'
 require 'logger'
 require 'redis'
 
-# Pusher.logger = Logger.new(STDOUT)
 $redis = Redis.connect(url: ENV.fetch("REDIS_URL", ENV.fetch("REDISTOGO_URL", "redis://localhost:6379/0")))
-
-# PUSHER_URL = ENV['PUSHER_URL'] || 'http://8d1a554cc7664a43eed1:0c00881f89481c483193@api.pusherapp.com/apps/33682'
-# pusher_url = URI.parse PUSHER_URL
-# PUSHER_KEY = pusher_url.user
-# PUSHER_SECRET = pusher_url.password
-# PUSHER_APP_ID = pusher_url.path.split('/').last
-
-# $pusher_client = Pusher::Client.new({
-#   app_id: PUSHER_APP_ID,
-#   key: PUSHER_KEY,
-#   secret: PUSHER_SECRET
-# })
 
 class FireState
   REDIS_KEY = "fire_state"
@@ -26,8 +13,17 @@ class FireState
     $redis.get(REDIS_KEY) || DEFAULT_STATE
   end
 
-  def set(value)
-    $redis.set(REDIS_KEY, value)
+  def set(red, orange, green)
+    state = [
+      red     ? '1': '0',
+      orange  ? '1': '0',
+      green   ? '1': '0'
+    ].join('')
+    p set: state
+    $redis.set(REDIS_KEY, state)
+  end
+
+  def set_colors(red, orange, green)
   end
 
 end
@@ -47,36 +43,13 @@ class Server < Sinatra::Base
     red =    parse_param_state params['red']
     orange = parse_param_state params['orange']
     green =  parse_param_state params['green']
-
-    # state = {red: red, orange: orange, green: green}
-
-    state = [
-      red == 'on' ? '1': '0',
-      orange == 'on' ? '1': '0',
-      green == 'on' ? '1': '0'
-    ].join('')
-    p state
-    FireState.new.set(state)
-  end
-
-  post "/instructions" do
-    puts params.inspect
-    instruction = params['instruction']
-    result = nil #send_pusher_instruction instruction
-    "Instruction #{instruction.inspect} sent to Pusher with result #{result.inspect}"
-  end
-
-  post "/pusher_hooks" do
-    puts params.inspect
-    "OK !"
+    FireState.new.set(red, orange, green)
   end
 
   def parse_param_state(param)
     case param
-    when 'on' then 'on'
-    when 'off' then 'off'
-    when 'blinking' then 'blinking'
-    else 'off'
+    when 'on' then true
+    else false
     end
   end
 end
@@ -85,22 +58,6 @@ CI_URL = ENV['CI_URL'] || "http://mathieu:pyroti@ci.preplaysports.com/go/cctray.
 CI_USERNAME = ENV['CI_USERNAME'] || 'mathieu'
 CI_PASSWORD = ENV['CI_PASSWORD']
 CI_PROJECT  = ENV['CI_PROJECT']  || "PPS :: tests :: tests"
-
-def send_pusher_instruction(instruction, data=nil)
-  p nil # $pusher_client.trigger(['instructions'], instruction, data || {})
-end
-
-def send_lights_state(red, orange, green)
-  state = [red, orange, green].map { |s| s == "on" || s == true ? 'y' : 'n' }.join("")
-
-  return if $fire_state == state
-
-  puts "Sending state #{state}"
-  send_pusher_instruction('state', state)
-  $fire_state = state
-  $redis.set(REDIS_KEY, $fire_state)
-  puts "Done."
-end
 
 def check_ci
   puts "Getting CI status..."
@@ -121,22 +78,16 @@ def check_ci
   green = last_status == 'Success'
   p [red, orange, green]
 
-  send_lights_state(red, orange, green)
+  FireState.new.set(red, orange, green)
 end
 
 STATES = %w(on off) #blinking
 
 # Thread.new do
-#   puts "CI polling / Pusher thread created."
+#   puts "CI polling thread created."
 #
 #   loop do
 #     begin
-#       # instruction = %w(red_on orange_on green_on red_off orange_off green_off).sample
-#       # puts "Sending instruction #{instruction.inspect} to Pusher..."
-#       # send_pusher_instruction instruction
-#
-#       # send_lights_state(STATES.sample, STATES.sample, STATES.sample)
-#
 #       check_ci
 #       sleep(2)
 #     rescue => e
